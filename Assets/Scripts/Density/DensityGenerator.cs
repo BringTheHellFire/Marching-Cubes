5ccb4808cb1ad710a72798c59303f9c51664e40d
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2023-10-29 12:29:44 +0100

    Add Tooltips for NoiseDensity

--- Assets/Scripts/Density/DensityGenerator.cs
+++ Assets/Scripts/Density/DensityGenerator.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public abstract class DensityGenerator : MonoBehaviour {
 
     const int threadGroupSize = 8;
+
+    private MeshGenerator meshGenerator;
+
+    [Header("Density Calculator")]
     public ComputeShader densityShader;
 
     protected List<ComputeBuffer> buffersToRelease;
+
+    private void Awake()
+    {
+        meshGenerator = FindObjectOfType<MeshGenerator>();
+    }
 
     void OnValidate() {
         if (FindObjectOfType<MeshGenerator>()) {
@@ -15,29 +24,37 @@ public abstract class DensityGenerator : MonoBehaviour {
         }
     }
 
-    public virtual ComputeBuffer Generate (ComputeBuffer pointsBuffer, int numPointsPerAxis, float boundsSize, Vector3 worldBounds, Vector3 centre, Vector3 offset, float spacing) {
+    public virtual ComputeBuffer Generate (ComputeBuffer pointsBuffer, int numPointsPerAxis, float boundsSize, Vector3 worldBounds, Vector3 centre, Vector3 offset, float spacing)
+    {
         int numPoints = numPointsPerAxis * numPointsPerAxis * numPointsPerAxis;
-        int numThreadsPerAxis = Mathf.CeilToInt (numPointsPerAxis / (float) threadGroupSize);
+        int numThreadsPerAxis = Mathf.CeilToInt(numPointsPerAxis / (float)threadGroupSize);
         // Points buffer is populated inside shader with pos (xyz) + density (w).
         // Set paramaters
-        densityShader.SetBuffer (0, "points", pointsBuffer);
-        densityShader.SetInt ("numPointsPerAxis", numPointsPerAxis);
-        densityShader.SetFloat ("boundsSize", boundsSize);
-        densityShader.SetVector ("centre", new Vector4 (centre.x, centre.y, centre.z));
-        densityShader.SetVector ("offset", new Vector4 (offset.x, offset.y, offset.z));
-        densityShader.SetFloat ("spacing", spacing);
+        densityShader.SetBuffer(0, "points", pointsBuffer);
+        densityShader.SetInt("numPointsPerAxis", numPointsPerAxis);
+        densityShader.SetFloat("boundsSize", boundsSize);
+        densityShader.SetVector("centre", new Vector4(centre.x, centre.y, centre.z));
+        densityShader.SetVector("offset", new Vector4(offset.x, offset.y, offset.z));
+        densityShader.SetFloat("spacing", spacing);
         densityShader.SetVector("worldSize", worldBounds);
 
         // Dispatch shader
-        densityShader.Dispatch (0, numThreadsPerAxis, numThreadsPerAxis, numThreadsPerAxis);
+        densityShader.Dispatch(0, numThreadsPerAxis, numThreadsPerAxis, numThreadsPerAxis);
 
-        if (buffersToRelease != null) {
-            foreach (var b in buffersToRelease) {
-                b.Release();
-            }
-        }
+        ReleaseBuffers();
 
         // Return voxel data buffer so it can be used to generate mesh
         return pointsBuffer;
+    }
+
+    private void ReleaseBuffers()
+    {
+        if (buffersToRelease != null)
+        {
+            foreach (var b in buffersToRelease)
+            {
+                b.Release();
+            }
+        }
     }
 }
--- Assets/Scripts/Density/NoiseDensity.cs
+++ Assets/Scripts/Density/NoiseDensity.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoiseDensity : DensityGenerator {
-
-    [Header ("Noise")]
+public class NoiseDensity : DensityGenerator 
+{
+    [Header ("Noise Settings")]
     public int seed;
+    [Tooltip("Number of layers of the noise. For making finer details.")]
     public int numOctaves = 4;
+    [Tooltip("The frequency of each octave. For making the noise more detailed or smoother.")]
     public float lacunarity = 2;
+    [Tooltip("Amplitude of each octave. A value less than 1 decreases the contirbution of each subsequen octave.")]
     public float persistence = .5f;
+    [Tooltip("This scales the noise value, effectively zooming in or out on the noise pattern.")]
     public float noiseScale = 1;
+    [Tooltip("This weights the noise value, allowing for a heavier or lighter influence of the noise on the overall density.")]
     public float noiseWeight = 1;
+    [Tooltip("If set to true, it ensures that the edges of the density field are 'closed' or 'sealed'.")]
     public bool closeEdges;
+    [Tooltip("Used to raise or lower the base height of the noise, effectively acting as a vertical offset.")]
     public float floorOffset = 1;
+    [Tooltip("Weight multiplier for the floor.")]
     public float weightMultiplier = 1;
-
+    [Tooltip("At what height (or depth) the density is adjusted more sharply, creating a 'hard' floor.")]
     public float hardFloorHeight;
+    [Tooltip("Specifies how 'hard' or strong the effect of the hard floor is on the density.")]
     public float hardFloorWeight;
 
+    [Tooltip("")]
     public Vector4 shaderParams;
 
     public override ComputeBuffer Generate (ComputeBuffer pointsBuffer, int numPointsPerAxis, float boundsSize, Vector3 worldBounds, Vector3 centre, Vector3 offset, float spacing) {
