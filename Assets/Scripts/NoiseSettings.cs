c3e5e754a142c7bb994430638c7ec79d2cc189a9
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2023-11-02 18:34:51 +0100

    Refactor NoiseSettings to adjust ranges and prepare for exposed properties

--- Assets/Scripts/NoiseSettings.cs
+++ Assets/Scripts/NoiseSettings.cs
@@ -5,28 +5,42 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseSettings
 {
-    [Header("Noise Settings")]
+    [Header("General Settings")]
+    [Tooltip("The seed of the generation.")]
     public int seed;
+    [Tooltip("If set to true, it ensures that the edges of the density field are 'closed' or 'sealed'.")]
+    public bool closeEdges;
     [Tooltip("Number of layers of the noise. For making finer details.")]
     public int numOctaves = 4;
     [Tooltip("The frequency of each octave. For making the noise more detailed or smoother.")]
     public float lacunarity = 2;
     [Tooltip("Amplitude of each octave. A value less than 1 decreases the contirbution of each subsequen octave.")]
     public float persistence = .5f;
+
+    [Header("Randomized Properties")]
     [Tooltip("This scales the noise value, effectively zooming in or out on the noise pattern.")]
     public float noiseScale = 1;
+    public PropertyRange noiseScaleRandomizationRange;
+    [Space(2)]
     [Tooltip("This weights the noise value, allowing for a heavier or lighter influence of the noise on the overall density.")]
     public float noiseWeight = 1;
-    [Tooltip("If set to true, it ensures that the edges of the density field are 'closed' or 'sealed'.")]
-    public bool closeEdges;
+    public PropertyRange noiseWeightRandomizationRange;
+    [Space(2)]
     [Tooltip("Used to raise or lower the base height of the noise, effectively acting as a vertical offset.")]
     public float floorOffset = 1;
+    public PropertyRange floorOffsetRandomizationRange;
+    [Space(2)]
     [Tooltip("Weight multiplier for the floor.")]
     public float weightMultiplier = 1;
+    public PropertyRange weightMultiplierRandomizationRange;
+    [Space(2)]
     [Tooltip("At what height (or depth) the density is adjusted more sharply, creating a 'hard' floor.")]
     public float hardFloorHeight;
+    public PropertyRange hardFloorHeightRandomizationRange;
+    [Space(2)]
     [Tooltip("Specifies how 'hard' or strong the effect of the hard floor is on the density.")]
     public float hardFloorWeight;
+    public PropertyRange hardFloorWeightRandomizationRange;
 
     public static NoiseSettings Lerp(NoiseSettings a, NoiseSettings b, float t)
     {
@@ -37,7 +51,7 @@ public class NoiseSettings
         result.persistence = Mathf.Lerp(a.persistence, b.persistence, t);
         result.noiseScale = Mathf.Lerp(a.noiseScale, b.noiseScale, t);
         result.noiseWeight = Mathf.Lerp(a.noiseWeight, b.noiseWeight, t);
-        result.closeEdges = t < 0.5f ? a.closeEdges : b.closeEdges; // Use the closeEdges value from one of the settings.
+        result.closeEdges = t < 0.5f ? a.closeEdges : b.closeEdges;
         result.floorOffset = Mathf.Lerp(a.floorOffset, b.floorOffset, t);
         result.weightMultiplier = Mathf.Lerp(a.weightMultiplier, b.weightMultiplier, t);
         result.hardFloorHeight = Mathf.Lerp(a.hardFloorHeight, b.hardFloorHeight, t);
@@ -49,19 +63,27 @@ public class NoiseSettings
     {
         NoiseSettings randomSettings = new NoiseSettings();
 
-        // Set the range for each property based on your requirements.
         randomSettings.seed = 1;
-        randomSettings.numOctaves = 2;
-        randomSettings.lacunarity = Random.Range(1.5f, 3.0f); // Adjust the range as needed.
-        randomSettings.persistence = Random.Range(0.3f, 0.5f); // Adjust the range as needed.
-        randomSettings.noiseScale = Random.Range(0.5f, 10f); // Adjust the range as needed.
-        randomSettings.noiseWeight = Random.Range(0.8f, 20f); // Adjust the range as needed.
         randomSettings.closeEdges = true;
-        randomSettings.floorOffset = Random.Range(0.5f, 1.5f); // Adjust the range as needed.
-        randomSettings.weightMultiplier = Random.Range(0.8f, 1.2f); // Adjust the range as needed.
-        randomSettings.hardFloorHeight = Random.Range(0.0f, 10f); // Adjust the range as needed.
-        randomSettings.hardFloorWeight = Random.Range(0.5f, 10f); // Adjust the range as needed.
+        randomSettings.numOctaves = 8;
+        randomSettings.lacunarity = 2f;
+        randomSettings.persistence = 0.54f;
+        randomSettings.noiseScale = Random.Range(0.5f, 5f);
+        randomSettings.noiseWeight = Random.Range(0.1f, 7f);
+        randomSettings.floorOffset = Random.Range(0.5f, 1.5f);
+        randomSettings.weightMultiplier = Random.Range(0.8f, 1.2f);
+        randomSettings.hardFloorHeight = Random.Range(-5f, 10f);
+        randomSettings.hardFloorWeight = Random.Range(0.5f, 10f);
 
         return randomSettings;
     }
 }
+
+[System.Serializable]
+public struct PropertyRange
+{
+    public float minValue;
+    public float maxValue;
+}
+
+
